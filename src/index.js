05f5701c9c2b3d8578c4608cead6c6ca4e930852
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2023-06-04 17:26:44 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import {PORT} from './config.js'
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json())
 
 app.use(indexRoutes)
