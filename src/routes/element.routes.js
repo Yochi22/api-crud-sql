fa0b34b64e26adfae2f6109a74b8f6ccdb42b06d
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2023-06-05 08:58:35 -0600

    prueba

--- src/routes/element.routes.js
+++ src/routes/element.routes.js
@@ -8,6 +8,6 @@ router.get('/categories/:id/elements/:id', getElements)
 router.get('/elements/:id', getElement)
 router.patch('/elements/:id', putElements)
 router.post('/categories/:id/elements', postElements )
-router.delete('/categories/:id/elements/:id', deleteElements)
+router.delete('/elements/:id', deleteElements)
 
 export default router
